bb117d776db914b7696dcbc2d39a264d1be23edc
Mick van Gelderen <mickvangelderen@gmail.com>
2015-10-01 14:16:01 +0200

    Moved adhere.each to core package as adhere.array

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-var adhere = module.exports = require('adhere-core')
+module.exports = require('adhere-core')
 
 adhere.clone = require('./lib/clone')
-adhere.each = require('./lib/each')
 adhere.expectify = require('./lib/expectify')
 adhere.filterify = require('./lib/filterify')
 adhere.is = require('./lib/is')
