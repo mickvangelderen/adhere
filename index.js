1b482b4adea517bc2522ae6cba601b8e56c1106e
Mick van Gelderen <mickvangelderen@gmail.com>
2015-10-08 12:38:58 +0200

    Added tests for the adhere object.

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-module.exports = require('adhere-core')
-
-adhere.clone = require('./lib/clone')
-adhere.expectify = require('./lib/expectify')
-adhere.filterify = require('./lib/filterify')
-adhere.is = require('./lib/is')
+module.exports = require('./lib')
